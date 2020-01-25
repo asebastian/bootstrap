b4b7b3451327a9884e0f8f8e6cae3ab85bc17046
asebastian <asebastian2@gmail.com>
2020-01-25 15:13:17 -0500

    Colorize and limit output of bootstrap-ubuntu.sh

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -1,73 +1,78 @@
-#!/bin/bash -xe
-
+#!/bin/bash -e
 
 # TODO: All commands need to use the user who executed the script with sudo's
 # home directory, not root.
 if [ "$EUID" -ne 0 ]
-  then echo "This script needs to be run with sudo or as root, exiting"
+  then echo -e "\e[31m[!] This script needs to be run with sudo or as root, exiting"
   exit
 fi
 
-echo "==> Installing basic tools"
-snap install ripgrep --classic
-snap alias ripgrep.rg rg
+echo -e "\e[92m==> Installing basic tools available via snap"
+snap install ripgrep --classic &> /dev/null
+snap alias ripgrep.rg rg &> /dev/null
 
-echo "==> Installing zsh"
-apt-get install -q -y zsh
+echo -e "\e[92m==> Installing basic tools available via apt"
+apt-get install -q -y zsh vim tmux git wget curl build-essential &> /dev/null
+
+echo -e "\e[33m==> Configuring zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/.zshrc -O ~/.zshrc &> /dev/null
 
-echo "==> Configuring zsh"
-wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/.zshrc -O ~/.zshrc
-
-echo "==> Installing go"
+# Compose the latest go archive url for linux-amd64.
 export LATEST_GO_VERSION=$(curl 'https://golang.org/VERSION?m=text')
 export GO_ARCHIVE="$LATEST_GO_VERSION.linux-amd64.tar.gz"
 export GO_ARCHIVE_URL="https://dl.google.com/go/$GO_ARCHIVE"
 
-wget -q $GO_ARCHIVE_URL
-tar xzf $GO_ARCHIVE
-mv go /usr/local/
+echo -e "\e[34m==> Latest version of go is $LATEST_GO_VERSION, downloading from $GO_ARCHIVE_URL..."
+wget -q $GO_ARCHIVE_URL &> /dev/null
 
-rm $GO_ARCHIVE
+echo -e "\e[92m==> Installing $LATEST_GO_VERSION to /usr/local/go"
+tar xzf $GO_ARCHIVE &> /dev/null
+mv go /usr/local/ &> /dev/null
 
-echo "==> Installing vim"
-apt-get install vim
+echo -e "\e[34m==> Go toolchain install was successful, removing temporary artifacts"
+rm -rf $GO_ARCHIVE &> /dev/null
 
-echo "==> Configuring vim"
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo -e "\e[33m==> Configuring vim"
+mkdir -p ~/.vim/autoload ~/.vim/bundle &> /dev/null
 
-git clone https://github.com/elixir-lang/vim-elixir.git ~/.vim/bundle/vim-elixir
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-git clone https://github.com/pearofducks/ansible-vim ~/.vim/bundle/ansible-vim
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/thaerkh/vim-workspace ~/.vim/bundle/vim-workspace
-git clone https://github.com/wincent/terminus.git ~/.vim/bundle/terminus
+echo -e "\e[34m==> Vim: configuring pathogen"
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &> /dev/null
 
-echo "==> Installing and configuring git"
-apt-get install -q -y git
-wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/git/.gitconfig
-wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/git/.githelpers
-wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/git/.gitignore
+echo -e "\e[34m==> Vim: installing vim-elixir, vim-airline, vim-airline-themes, ansible-vim, vim-gitgutter, vim-workspace and terminus"
+git clone https://github.com/elixir-lang/vim-elixir.git ~/.vim/bundle/vim-elixir &> /dev/null
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline &> /dev/null
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes &> /dev/null
+git clone https://github.com/pearofducks/ansible-vim ~/.vim/bundle/ansible-vim &> /dev/null
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter &> /dev/null
+git clone https://github.com/thaerkh/vim-workspace ~/.vim/bundle/vim-workspace &> /dev/null
+git clone https://github.com/wincent/terminus.git ~/.vim/bundle/terminus &> /dev/null
 
+echo -e "\e[34m==> Vim: performing final configuration operations"
 mkdir -p ~/.vim/colors
-wget -q https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim -P ~/.vim/colors/
+wget -q https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim -P ~/.vim/colors/ &> /dev/null
+wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/.vimrc &> /dev/null
 
-wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/.vimrc
-
-echo "==> Installing/configuring tmux"
-apt-get install -q -y tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/.tmux.conf
-echo "==> Enter tmux then hit Ctrl-b (leader) + I to automatically configure the plugins"
-
-echo "==> Installing vim-go dependencies, this may take awhile..."
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+echo -e "\e[34m==> Installing vim-go dependencies, this may take awhile..."
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go &> /dev/null
 vim -c 'GoInstallBinaries' -c 'qa!'
 
-echo "==> Installing fzf from source..."
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
 
-chsh -s /bin/zsh
-echo "==> Bootstrap complete! Log out and log back in to use zsh."
+echo -e "\e[33m==> Configuring git"
+wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/git/.gitconfig &> /dev/null
+wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/git/.githelpers &> /dev/null
+wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/git/.gitignore &> /dev/null
+
+echo -e "\e[33m==> Configuring tmux"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &> /dev/null
+wget -q https://raw.githubusercontent.com/asebastian/bootstrap/master/files/.tmux.conf &> /dev/null
+
+echo -e "\e[92m==> Installing fzf from source..."
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &> /dev/null
+~/.fzf/install &> /dev/null
+
+echo -e "\e[33m==> Setting the default shell to zsh for $(whoami)"
+chsh -s /bin/zsh &> /dev/null
+
+echo -e "\e[96m==> One last thing to do for tmux (optional)! Enter tmux then hit Ctrl-b (leader) + I to automatically configure the plugins"
+echo -e "\e[92m==> Bootstrap complete! Log out and log back in to use zsh."
